3c788c639f6b191c585fb7891f9d451ca089aa9e
Hemanth Raj <smhemanth2002@gmail.com>
2024-05-15 13:30:44 +0530

    complete

---
+++ app.py
@@ -0,0 +1,29 @@
+from flask import Flask, render_template, url_for, send_file, request, flash
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    about_img = url_for('static', filename='images/about.jpg')
+    bg_img = url_for('static', filename='images/bg_1.jpg')
+    return render_template('index.html', about_img = about_img, bg_img = bg_img)
+
+@app.route('/contact')
+def contact():
+    return render_template('form.html')
+
+
+@app.route('/download_file')
+def download_file():
+    path="RESUME.pdf"
+    return send_file(path,as_attachment=True)
+
+@app.route('/project')
+def project():
+    return render_template('project.html')
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+if __name__ == '__main__':
+    app.run('0.0.0.0', port=5000)
